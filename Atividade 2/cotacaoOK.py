f2bfc870c26b25137d3a56f7a86a1afb6f5fd5be
Guilherme Veloso <guilherme_veloso1@estudante.sesisenai.org.br>
2024-05-07 21:28:16 -0300

    att banco

--- Atividade 2/cotacaoOK.py
+++ Atividade 2/cotacaoOK.py
@@ -1,45 +1,104 @@
 import mysql.connector
 from mysql.connector import Error
+from datetime import datetime
 import PySimpleGUI as sg
-
-# Conectar ao banco de dados
-mydb = mysql.connector.connect(
-  host="localhost",
-  user="seu_usuario",
-  password="sua_senha",
-  database="COTACAO"
-)
-cursor = mydb.cursor()
-
-# Função para buscar cotação no banco de dados
-def buscar_cotacao(moeda):
-    query = f"SELECT valor_moeda FROM MOEDA WHERE moeda = '{moeda}'"
-    cursor.execute(query)
-    result = cursor.fetchone()
-    if result:
-        return result[0]
-    else:
-        return None
-
-# Função para atualizar cotação no banco de dados
-def atualizar_cotacao(moeda, valor):
-    query = f"UPDATE MOEDA SET valor_moeda = '{valor}' WHERE moeda = '{moeda}'"
-    cursor.execute(query)
-    mydb.commit()
-
-def criarConexao(host_name, user_name, user_password):
-    connection = None
+sg.theme('Reddit')
+def criarDatabase(host_name, user_name, user_password, db_name):
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
             passwd=user_password
         )
-        print("MySQL Database connection successful")
+        cursor = connection.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+        cursor.execute(f"USE {db_name}")
+        print("Database created successfully")
+        return connection
     except Error as err:
         print(f"Error: '{err}'")
 
-    return connection
+def criarTabela(connection, query):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print("Table created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+def execute_query(connection, query):
+    cursor = connection.cursor()
+    try:
+        cursor.execute(query)
+        connection.commit()
+        print("Query successful")
+    except Error as err:
+        print(f"Error: '{err}'")
+# Criar o banco de dados 'cotacao' se ele não existir
+connection = criarDatabase("localhost", "root", "root", "cotacao")
+
+# Criação da tabela moeda
+query = '''
+    CREATE TABLE IF NOT EXISTS moeda (
+        idMOEDA INT PRIMARY KEY, 
+        moeda VARCHAR(50),
+        valor_moeda DOUBLE
+    )
+'''
+criarTabela(connection, query)
+
+# Criação da tabela conversao
+query = '''
+    CREATE TABLE IF NOT EXISTS conversao (
+        idCONVERSAO INT AUTO_INCREMENT PRIMARY KEY, 
+        valor_real DOUBLE,
+        valor_convertido DOUBLE,
+        data_hora DATETIME,
+        MOEDA_idMOEDA INT NOT NULL,
+        FOREIGN KEY (MOEDA_idMOEDA)
+        REFERENCES moeda (idMOEDA)
+        ON DELETE CASCADE
+        ON UPDATE CASCADE
+    )
+'''
+criarTabela(connection, query)
+
+# Inserção de dados na tabela moeda
+query = '''
+    INSERT INTO moeda VALUES
+        (1, 'Dolar', 7),
+        (2, 'Bitcoin', 2),
+        (3, 'Etherium', 4),
+        (4, 'Euro', 1),
+        (5, 'Libra', 3),
+        (6, 'Iene', 3)
+'''
+criarTabela(connection, query)
+connection.commit()
+
+# Consulta à tabela moeda
+query = '''
+    SELECT * FROM moeda
+'''
+cursor = connection.cursor()
+cursor.execute(query)
+result = cursor.fetchall()
+
+# Função para buscar cotação no banco de dados
+def buscar_cotacao(moeda):
+    query = f"SELECT valor_moeda FROM moeda WHERE moeda = '{moeda}'"
+    cursor.execute(query)
+    result = cursor.fetchone()
+    if result:
+        return result[0]
+    else:
+        return None
+# Função para atualizar cotação no banco de dados
+def atualizar_cotacao(moeda, valor):
+    query = f"UPDATE moeda SET valor_moeda = '{valor}' WHERE moeda = '{moeda}'"
+    cursor.execute(query)
+    mydb.commit()
+
 def paginaValor():
     moeda = ''
     cotacao = 0.0
@@ -66,7 +125,7 @@ def paginaValor():
         if(event == sg.WIN_CLOSED):
             window.close()
             break
-         #while True:
+        while True:
             event, values = window.read()
             if(event == sg.WIN_CLOSED):
                 window.close()
@@ -82,14 +141,15 @@ def paginaValor():
 
         if(event == 'USD'):
             moeda = 'Dólar'
-            cotacao_dolar = 
+            cotacao_dolar = buscar_cotacao(moeda)
+            print(buscar_cotacao(moeda))
             cotacao = cotacao_dolar
             cotacao = str(cotacao).replace('.',',')
             window['moeda'].update(moeda)
             window['cotacao'].update("R$"+cotacao)
         if(event == 'BTC'):
             moeda = 'Bitcoin'
-            cotacao_bitcoin = #\request valor my sql
+            cotacao_bitcoin = buscar_cotacao(moeda)
             cotacao = float(cotacao_bitcoin)
             cotacao = f'{cotacao:_.3f}'
             cotacao = cotacao.replace('.',',').replace('_','.')
@@ -97,7 +157,7 @@ def paginaValor():
             window['cotacao'].update("R$"+cotacao)
         if(event == 'ETH'):
             moeda = 'Etherium'
-            cotacao_etherium = #\request valor my sql
+            cotacao_etherium = buscar_cotacao(moeda)
             cotacao = float(cotacao_etherium)
             cotacao = f'{cotacao:_.2f}'
             cotacao = cotacao.replace('.',',').replace('_','.')
@@ -105,21 +165,21 @@ def paginaValor():
             window['cotacao'].update("R$"+cotacao)
         if(event == 'EUR'):
             moeda = 'Euro'
-            cotacao_euro = #\request valor my sql
+            cotacao_euro = buscar_cotacao(moeda)
             cotacao = cotacao_euro
             cotacao = str(cotacao).replace('.',',')
             window['moeda'].update(moeda)
             window['cotacao'].update("R$"+cotacao)
         if(event == 'GBP'):
             moeda = 'Libra'
-            cotacao_libra = #\request valor my sql
+            cotacao_libra = buscar_cotacao(moeda)
             cotacao = cotacao_libra
             cotacao = str(cotacao).replace('.',',')
             window['moeda'].update(moeda)
             window['cotacao'].update("R$"+cotacao)
         if(event == 'JPY'):
             moeda = 'Iene'
-            cotacao_iene = #\request valor my sql
+            cotacao_iene = buscar_cotacao(moeda)
             cotacao = cotacao_iene
             cotacao = str(cotacao).replace('.',',')
             window['moeda'].update(moeda)
@@ -168,66 +228,111 @@ def paginaConversao():
     window = sg.Window('registro', layout, size=(500,500), element_justification='center')
     while True:
         event, values = window.read()
-        valorReais = float(values[0])
+        valorReais = values[0]
         if(event == sg.WIN_CLOSED):
             window.close()
             break
         if(event == 'USD'):
             moeda = 'Dólar'
-            cotacao_dolar = #\request valor my sql
+            data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            print(data_atual)
+            cotacao_dolar = buscar_cotacao(moeda)
             valorConversao = float(valorReais) / float(cotacao_dolar)  
             valorConversao = str(valorConversao)
             window['moeda'].update(moeda)
             window['cotacao'].update("$"+valorConversao)
+            query = f'''
+                INSERT INTO conversao (valor_real, valor_convertido, data_hora, MOEDA_idMOEDA) VALUES
+                ({valorReais}, {valorConversao}, '{data_atual}', 1)
+                '''
+            criarTabela(connection, query)
+            connection.commit()
         if(event == 'BTC'):
             moeda = 'Bitcoin'
-            cotacao_bitcoin = #\request valor my sql
+            data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cotacao_bitcoin = buscar_cotacao(moeda)
             valorConversao =  float(valorReais) / float(cotacao_bitcoin)
             valorConversao = str(valorConversao)  
             window['moeda'].update(moeda)
             window['cotacao'].update("BTC "+valorConversao)
+            query = f'''
+                INSERT INTO conversao (valor_real, valor_convertido, data_hora, MOEDA_idMOEDA) VALUES
+                ({valorReais}, {valorConversao}, '{data_atual}', 2)
+                '''
+            criarTabela(connection, query)
+            connection.commit()
         if(event == 'ETH'):
             moeda = 'Etherium'
-            cotacao_etherium = #\request valor my sql
+            data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cotacao_etherium = buscar_cotacao(moeda)
             valorConversao =  float(valorReais) / float(cotacao_etherium)
             valorConversao = str(valorConversao)
             window['moeda'].update(moeda)
             window['cotacao'].update("ETH "+valorConversao)
+            query = f'''
+                INSERT INTO conversao (valor_real, valor_convertido, data_hora, MOEDA_idMOEDA) VALUES
+                ({valorReais}, {valorConversao}, '{data_atual}', 3)
+                '''
+            criarTabela(connection, query)
+            connection.commit()
         if(event == 'EUR'):
             moeda = 'Euro'
-            cotacao_euro = #\request valor my sql
+            data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cotacao_euro = buscar_cotacao(moeda)
             valorConversao =  float(valorReais) / float(cotacao_euro)
             valorConversao = str(valorConversao)
             window['moeda'].update(moeda)
             window['cotacao'].update("€"+valorConversao)
+            query = f'''
+                INSERT INTO conversao (valor_real, valor_convertido, data_hora, MOEDA_idMOEDA) VALUES
+                ({valorReais}, {valorConversao}, '{data_atual}', 4)
+                '''
+            criarTabela(connection, query)
+            connection.commit()
         if(event == 'GBP'):
             moeda = 'Libra'
-            cotacao_libra = #\request valor my sql
+            data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cotacao_libra = buscar_cotacao(moeda)
             valorConversao =  float(valorReais) / float(cotacao_libra)
             valorConversao = str(valorConversao)
             window['moeda'].update(moeda)
             window['cotacao'].update("£"+valorConversao)
+            query = f'''
+                INSERT INTO conversao (valor_real, valor_convertido, data_hora, MOEDA_idMOEDA) VALUES
+                ({valorReais}, {valorConversao}, '{data_atual}', 5)
+                '''
+            criarTabela(connection, query)
+            connection.commit()
         if(event == 'JPY'):
             moeda = 'Iene'
-            cotacao_iene = #\request valor my sql
+            data_atual = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+            cotacao_iene = buscar_cotacao(moeda)
             valorConversao =  float(valorReais) / float(cotacao_iene)
             valorConversao = str(valorConversao)
             window['moeda'].update(moeda)
             window['cotacao'].update("¥"+valorConversao)
+            query = f'''
+                INSERT INTO conversao (valor_real, valor_convertido, data_hora, MOEDA_idMOEDA) VALUES
+                ({valorReais}, {valorConversao}, '{data_atual}', 6)
+                '''
+            criarTabela(connection, query)
+            connection.commit()
 # Tema do PySimpleGUI
 
 
-# Funcionamento
-while True:
-    event, values = window.read()
-    if(event == sg.WIN_CLOSED):
-        window.close()
-        break
-    if(event in ['USD', 'EUR', 'GBP', 'JPY', 'BTC', 'ETH']):
-        moeda = event
-        cotacao = buscar_cotacao(moeda)
-        if cotacao is not None:
-            window['moeda'].update(f"Valor da moeda {moeda}")
-            window['cotacao'].update(f"R${cotacao}")
-        else:
-            sg.popup(f"Cotação da moeda {moeda} não encontrada no banco de dados")
+# while True:
+#     event, values = window.read()
+#     if(event == sg.WIN_CLOSED):
+#         window.close()
+#         break
+#     if(event in ['USD', 'EUR', 'GBP', 'JPY', 'BTC', 'ETH']):
+#         moeda = event
+#         cotacao = buscar_cotacao(moeda)
+#         if cotacao is not None:
+#             window['moeda'].update(f"Valor da moeda {moeda}")
+#             window['cotacao'].update(f"R${cotacao}")
+#         else:
+#             sg.popup(f"Cotação da moeda {moeda} não encontrada no banco de dados")
+
+
+paginaInicial()
--- teste.py
+++ teste.py
@@ -1,74 +1,84 @@
 import mysql.connector
 from mysql.connector import Error
-def criarConexao(host_name, user_name, user_password):
-    connection = None
+
+def criarDatabase(host_name, user_name, user_password, db_name):
     try:
         connection = mysql.connector.connect(
             host=host_name,
             user=user_name,
             passwd=user_password
         )
-        print("MySQL Database connection successful")
+        cursor = connection.cursor()
+        cursor.execute(f"CREATE DATABASE IF NOT EXISTS {db_name}")
+        cursor.execute(f"USE {db_name}")
+        print("Database created successfully")
+        return connection
     except Error as err:
         print(f"Error: '{err}'")
 
-    return connection
-def criarDatabase(connection, query):
+def criarTabela(connection, query):
+    try:
+        cursor = connection.cursor()
+        cursor.execute(query)
+        print("Table created successfully")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+def execute_query(connection, query):
     cursor = connection.cursor()
     try:
         cursor.execute(query)
-        print("Database created successfully")
+        connection.commit()
+        print("Query successful")
     except Error as err:
         print(f"Error: '{err}'")
-connection = criarConexao("localhost", "root", "root")
-criarDatabase_query = "CREATE DATABASE cotacao"
-criarDatabase(connection, criarDatabase_query)
+# Criar o banco de dados 'cotacao' se ele não existir
+connection = criarDatabase("localhost", "root", "root", "cotacao")
 
+# Criação da tabela moeda
 query = '''
-    CREATE TABLE moeda (
-        id_moeda INT PRIMARY KEY, 
+    CREATE TABLE IF NOT EXISTS moeda (
+        idMOEDA INT PRIMARY KEY, 
         moeda VARCHAR(50),
         valor_moeda DOUBLE
     )
 '''
-cursor = connection.cursor()
-cursor.execute(query)
+criarTabela(connection, query)
 
+# Criação da tabela conversao
 query = '''
-    CREATE TABLE conversao (
-        id_conversao INT PRIMARY KEY, 
-        valor_real    DOUBLE,
+    CREATE TABLE IF NOT EXISTS conversao (
+        idCONVERSAO INT PRIMARY KEY, 
+        valor_real DOUBLE,
         valor_convertido DOUBLE,
         data_hora DATETIME,
         MOEDA_idMOEDA INT NOT NULL,
         FOREIGN KEY (MOEDA_idMOEDA)
-	    REFERENCES MOEDA (idMOEDA)
+        REFERENCES moeda (idMOEDA)
         ON DELETE CASCADE
         ON UPDATE CASCADE
     )
 '''
-cursor = connection.cursor()
-cursor.execute(query)
+criarTabela(connection, query)
 
-
+# Inserção de dados na tabela moeda
 query = '''
     INSERT INTO moeda VALUES
-        (1, 'Dolar', '7'),
-        (2, 'Bitcoin', '2'),
-        (3, 'Etherium', '4'),
-        (4, 'Euro', '1),
-        (5, 'Libra', '3'),
-        (6, 'Iene', '3')
+        (1, 'Dolar', 7),
+        (2, 'Bitcoin', 2),
+        (3, 'Etherium', 4),
+        (4, 'Euro', 1),
+        (5, 'Libra', 3),
+        (6, 'Iene', 3)
 '''
-cursor = connection.cursor()
-cursor.execute(query)
+criarTabela(connection, query)
 connection.commit()
 
+# Consulta à tabela moeda
 query = '''
-    SELECT * FROM moedas
+    SELECT * FROM moeda
 '''
 cursor = connection.cursor()
 cursor.execute(query)
 result = cursor.fetchall()
-for row in result:
-
+print(result)
